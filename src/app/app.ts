720a72966de719cfc4503b900da8d40bf0e2856b
Tom <tom.petri7@gmail.com>
2025-09-03 16:50:48 +0200

    feat: Create and added why-me component Added styles to why-me component Added forgoten mixins with include to intro.scss file Added the variables $color-text-link and $gap-m to variables.scss file Moved the fonts.scss file from folder public/fonts to folder src Added the curved-arrow.svg icon

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from './components/header/header';
 import { Intro } from './components/intro/intro';
+import { WhyMe } from './components/why-me/why-me';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Intro],
+  imports: [Header, Intro, WhyMe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
