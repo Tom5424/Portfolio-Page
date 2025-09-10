4809faba7cc940307f2f0ef83e8933e35b03db22
Tom <tom.petri7@gmail.com>
2025-09-10 18:27:22 +0200

    refactor: Remove no longer usable code Remove no longer usable code in why-me.scss file Remove no longer usable code in my-skills.scss file and add some properties Add a p tag to my-skills.html file Add the scrollToActiveSection() method from scroll-spy-service.ts file to header.ts file Add some properties to scroll-spy-service.ts file Add the method scrollToActiveSection() to scroll-spy-service.ts file Add d-none class to styles.scss file

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, inject, WritableSignal } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ScrollSpyService } from '../../services/scroll-spy-service';
 import { RouterLink } from '@angular/router';
 
@@ -12,9 +12,8 @@ import { RouterLink } from '@angular/router';
 })
 
 
-export class Header {
+export class Header implements OnInit {
   scrollSpyService = inject(ScrollSpyService);
-  activeLink: WritableSignal<string> = this.scrollSpyService.activeSection;
   navigationItems: { name: string, href: string }[] = [
     { 'name': 'Why me', 'href': 'why-me' },
     { 'name': 'Skills', 'href': 'my-skills' },
@@ -23,12 +22,18 @@ export class Header {
   ];
 
 
+  ngOnInit(): void {
+    this.scrollSpyService.scrollToActiveSection();
+  }
+
+
   setActiveLink(selectedLink: string): void {
-    this.scrollSpyService.setActiveSection(selectedLink);
+    this.scrollSpyService.scrollToActiveSection();
+    this.scrollSpyService.activeLink = selectedLink;
   }
 
 
   getActiveLinkClass(selectedLink: string): string {
-    return this.activeLink() === selectedLink ? 'active-link' : '';
+    return this.scrollSpyService.activeLink === selectedLink ? 'active-link' : '';
   }
 }
--- src/app/services/scroll-spy-service.ts
+++ src/app/services/scroll-spy-service.ts
@@ -1,4 +1,6 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Injectable({
@@ -7,7 +9,11 @@ import { Injectable, signal, WritableSignal } from '@angular/core';
 
 
 export class ScrollSpyService {
+  activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  viewportScroller: ViewportScroller = inject(ViewportScroller);
   activeSection: WritableSignal<string> = signal<string>('');
+  sectionOffsets: { [key: string]: number } = { 'why-me': 300, 'my-skills': 100 };
+  activeLink: string = '';
 
 
   setActiveSection(section: string): void {
@@ -17,5 +23,16 @@ export class ScrollSpyService {
 
   clearActiveSection(): void {
     this.activeSection.set('');
+  }
+
+
+  scrollToActiveSection(): void {
+    this.activatedRoute.fragment.subscribe(fragment => {
+      if (fragment) {
+        this.activeLink = fragment;
+        const sectionOffset = this.sectionOffsets[fragment];
+        this.viewportScroller.setOffset([0, sectionOffset]);
+      }
+    });
   }
 }
