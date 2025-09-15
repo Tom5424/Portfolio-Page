8962226daf577edabfa70217c9e56ec95bff5f9b
Tom <tom.petri7@gmail.com>
2025-09-15 16:51:51 +0200

    feat: Create and add contact-me component Add styles to contact-me component Add some icons import the variables.sccs file to use it in mixin.sccs file Extend the hover mixin with the property font-weight and add the $weight parameter Add the input-field-and-textarea() mixin to mixin.scss file Add the $color-button-disabled variable to variable.scss file

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { Intro } from './components/intro/intro';
 import { WhyMe } from './components/why-me/why-me';
 import { MySkills } from './components/my-skills/my-skills';
 import { MyProjects } from './components/my-projects/my-projects';
+import { ContactMe } from './components/contact-me/contact-me';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Intro, WhyMe, MySkills, MyProjects],
+  imports: [Header, Intro, WhyMe, MySkills, MyProjects, ContactMe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/contact-me/contact-me.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-contact-me',
+  imports: [],
+  templateUrl: './contact-me.html',
+  styleUrl: './contact-me.scss'
+})
+
+
+export class ContactMe {
+
+}
