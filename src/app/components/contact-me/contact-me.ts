29cbfff8013b627c0a4ecea892254d03a5708520
Tom <tom.petri7@gmail.com>
2025-09-17 17:07:18 +0200

    feat: In contact-me component, add validation to the form Add validation messages in the UI Add styles to the validation messages Add the method submitForm() to contact-me.ts file Add the following variables to variables.sccs file: $color-error-message, $color-success-message and $font-size-s Change the value of the color property in input-field-and-textarea mixin from rgba(variables.$color-accent, 0.6) to variables.$color-accent Add the following icons: check.svg and exclamation-mark.svg

--- src/app/components/contact-me/contact-me.ts
+++ src/app/components/contact-me/contact-me.ts
@@ -1,14 +1,24 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  imports: [FormsModule, NgClass],
   templateUrl: './contact-me.html',
   styleUrl: './contact-me.scss'
 })
 
 
 export class ContactMe {
+  contactForm: { name: string, email: string, message: string, checkboxPrivacyPolicy: boolean } = { name: '', email: '', message: '', checkboxPrivacyPolicy: false };
 
+
+  submitForm(contactForm: NgForm): void {
+    if (contactForm.valid) {
+      this.contactForm = contactForm.value;
+      contactForm.resetForm();
+    }
+  }
 }
