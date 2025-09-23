b46760ca016eac948fd530be775903be848d6da4
Tom <tom.petri7@gmail.com>
2025-09-23 02:57:18 +0200

    feat: Create and add page-not-found component Add styles to page-not-found component Add page-not-found route to app.routes.ts file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { LegalNotice } from './components/legal-notice/legal-notice';
 import { PrivacyPolicy } from './components/privacy-policy/privacy-policy';
+import { PageNotFound } from './components/page-not-found/page-not-found';
 
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'legal-notice', component: LegalNotice },
     { path: 'privacy-policy', component: PrivacyPolicy },
+    { path: '**', component: PageNotFound },
 ];
