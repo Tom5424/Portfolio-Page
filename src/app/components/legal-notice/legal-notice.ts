a8344ef79cfcf6e6680551fe2da6246a66558786
Tom <tom.petri7@gmail.com>
2025-09-25 20:07:20 +0200

    feat: The texts in legal-notice.html file can now translate Import the TranslatePipe and the TranslateServices in legal-notice.ts file Add the TranslatePipe to legal-notice.html file Extend the de.json and en.json file with the legalNotice key

--- src/app/components/legal-notice/legal-notice.ts
+++ src/app/components/legal-notice/legal-notice.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Header } from "../header/header";
 import { Footer } from "../footer/footer";
+import { TranslatePipe } from "@ngx-translate/core";
+import { TranslateServices } from '../../services/translate-service';
 
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [Header, Footer],
+  imports: [Header, Footer, TranslatePipe],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss'
 })
 
 
-export class LegalNotice {
+export class LegalNotice implements OnInit {
+  translateService: TranslateServices = inject(TranslateServices);
 
+
+  ngOnInit(): void {
+    this.translateService.setLanguages();
+    this.translateService.loadLanguage();
+    this.translateService.useLanguage(this.translateService.activeLanguage);
+  }
 }
