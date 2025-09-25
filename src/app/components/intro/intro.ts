54ea4a348d9f557c8a0e42500394a9877f501dce
Tom <tom.petri7@gmail.com>
2025-09-25 15:28:48 +0200

    feat: The texts in intro.html file can now translate Import the TranslatePipe in intro.ts file Add the TranslatePipe to intro.html file Extend the de.json and en.json file with the intro key

--- src/app/components/intro/intro.ts
+++ src/app/components/intro/intro.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ScrollSpyService } from '../../services/scroll-spy-service';
+import { TranslatePipe } from "@ngx-translate/core";
 
 
 @Component({
   selector: 'app-intro',
-  imports: [RouterLink],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './intro.html',
   styleUrl: './intro.scss'
 })
