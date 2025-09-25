54092f810991499484952d9219acdb0ceeaa83ae
Tom <tom.petri7@gmail.com>
2025-09-25 19:49:35 +0200

    feat: The texts in page-not-found.html file can now translate Import the TranslatePipe and the TranslateServices in page-not-found.ts file Add the TranslatePipe to page-not-found.html file Extend the de.json and en.json file with the pageNotFound key

--- src/app/components/page-not-found/page-not-found.ts
+++ src/app/components/page-not-found/page-not-found.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Footer } from '../footer/footer';
 import { Header } from "../header/header";
+import { TranslatePipe } from "@ngx-translate/core";
+import { TranslateServices } from '../../services/translate-service';
 
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [Header, Footer],
+  imports: [Header, Footer, TranslatePipe],
   templateUrl: './page-not-found.html',
   styleUrl: './page-not-found.scss'
 })
 
 
-export class PageNotFound {
+export class PageNotFound implements OnInit {
+  translateService: TranslateServices = inject(TranslateServices);
 
+
+  ngOnInit(): void {
+    this.translateService.setLanguages();
+    this.translateService.loadLanguage();
+    this.translateService.useLanguage(this.translateService.activeLanguage);
+  }
 }
