d5227a024ec15d588626cef3ed5c2a3d7b0bcd03
Tom <tom.petri7@gmail.com>
2025-09-26 18:54:09 +0200

    feat: The texts in privacy-policy.html file can now translate Import the TranslatePipe and the TranslateServices in privacy-policy.ts file Add the TranslatePipe to privacy-policy.html file Extend the de.json and en.json file with the privacyPolicy key

--- src/app/components/privacy-policy/privacy-policy.ts
+++ src/app/components/privacy-policy/privacy-policy.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Header } from "../header/header";
 import { Footer } from "../footer/footer";
+import { TranslatePipe } from "@ngx-translate/core";
+import { TranslateServices } from '../../services/translate-service';
 
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [Header, Footer],
+  imports: [Header, Footer, TranslatePipe],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss'
 })
 
 
-export class PrivacyPolicy {
+export class PrivacyPolicy implements OnInit {
+  translateService: TranslateServices = inject(TranslateServices);
 
+
+  ngOnInit(): void {
+    this.translateService.setLanguages();
+    this.translateService.loadLanguage();
+    this.translateService.useLanguage(this.translateService.activeLanguage);
+  }
 }
