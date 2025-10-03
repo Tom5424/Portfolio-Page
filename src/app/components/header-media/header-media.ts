376f11150517d72f9aa5e5535c402102d1c01de0
Tom <tom.petri7@gmail.com>
2025-10-03 18:47:02 +0200

    feat: Add if click on the burger menu button, open a responsive menu Add the responsive menu to header-media.html file Add several classes to header-media.scss file Import NgClass, RouterLink, TranslatePipe to header-media.ts file Add several methods to header-media.ts file Add close.svg icon

--- src/app/components/header-media/header-media.ts
+++ src/app/components/header-media/header-media.ts
@@ -1,23 +1,45 @@
-import { Component, inject, input } from '@angular/core';
-import { Router } from '@angular/router';
+import { AfterViewInit, Component, inject, input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { TranslateServices } from '../../services/translate-service';
+import { NgClass } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
+import { ScrollSpyService } from '../../services/scroll-spy-service';
 
 
 @Component({
   selector: 'app-header-media',
-  imports: [],
+  imports: [NgClass, RouterLink, TranslatePipe],
   templateUrl: './header-media.html',
   styleUrl: './header-media.scss'
 })
 
 
-export class HeaderMedia {
+export class HeaderMedia implements AfterViewInit {
   router: Router = inject(Router);
+  translateService: TranslateServices = inject(TranslateServices);
+  scrollSpyService: ScrollSpyService = inject(ScrollSpyService);
   isOnHomePage = input<boolean>(false);
   menuIsOpen: boolean = false;
+  navigationItems: { name: string, href: string }[] = [
+    { 'name': 'header.whyMe', 'href': 'why-me' },
+    { 'name': 'header.skills', 'href': 'my-skills' },
+    { 'name': 'header.projects', 'href': 'my-projects' },
+    { 'name': 'header.contact', 'href': 'contact-me' },
+  ];
 
 
-  toggleMenu(): void {
-    this.menuIsOpen = !this.menuIsOpen;
+  ngAfterViewInit(): void {
+    this.scrollSpyService.scrollToActiveSection(25);
+  }
+
+
+  openMenu(): void {
+    this.menuIsOpen = true;
+  }
+
+
+  closeMenu(): void {
+    this.menuIsOpen = false;
   }
 
 
@@ -25,5 +47,30 @@ export class HeaderMedia {
     if (!this.isOnHomePage()) {
       this.router.navigateByUrl('/');
     }
+  }
+
+
+  setActiveLink(selectedLink: string): void {
+    this.scrollSpyService.scrollToActiveSection(25);
+    this.scrollSpyService.activeLink = selectedLink;
+  }
+
+
+  getActiveLinkClass(selectedLink: string): string {
+    return this.scrollSpyService.activeLink === selectedLink ? 'active-link' : '';
+  }
+
+
+  selectLanguage(selectedLanguage: string): void {
+    if (this.translateService.activeLanguage !== selectedLanguage) {
+      this.translateService.activeLanguage = selectedLanguage;
+      this.translateService.saveLanguage(selectedLanguage);
+      this.translateService.useLanguage(selectedLanguage);
+    }
+  }
+
+
+  getActiveLanguageClass(selectedLanguage: string): string {
+    return this.translateService.activeLanguage === selectedLanguage ? 'active-language' : 'inactive-language';
   }
 }
